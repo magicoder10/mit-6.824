0c6039fc25d09768885577dae8e62a366d64e694
Magic Coder <magicoder20@gmail.com>
2023-12-10 01:53:44 -0800

    Update log pattern

--- src/analyze/pattern.go
+++ src/analyze/pattern.go
@@ -9,10 +9,10 @@ var runningTasksPattern = regexp.MustCompile("runningTasks=map\\[([a-zA-Z0-9.: ]
 
 var taskCountPattern = regexp.MustCompile("([a-zA-Z0-9.]+:\\d+):(\\d+)")
 
-var connectPattern = regexp.MustCompile("^connect\\(([0-9]+)\\)")
-var disconnectPattern = regexp.MustCompile("^disconnect\\(([0-9]+)\\)")
+var connectPattern = regexp.MustCompile("\\sconnect\\(([0-9]+)\\)")
+var disconnectPattern = regexp.MustCompile("\\sdisconnect\\(([0-9]+)\\)")
 
-var startPattern = regexp.MustCompile("^start1\\(([0-9]+)\\)")
-var crashPattern = regexp.MustCompile("^crash1\\(([0-9]+)\\)")
-var networkStatePattern = regexp.MustCompile("^([0-9]+)  (connect|disconnect)\\([0-9]+\\) map\\[([0-9: a-zA-Z]+)]$")
+var startPattern = regexp.MustCompile("\\sstart1\\(([0-9]+)\\)")
+var crashPattern = regexp.MustCompile("\\scrash1\\(([0-9]+)\\)")
+var networkStatePattern = regexp.MustCompile("^([0-9]+)  [0-9:.]+ (connect|disconnect)\\([0-9]+\\) map\\[([0-9: a-zA-Z]+)]$")
 var serverConnectionStatePattern = regexp.MustCompile("([0-9]+):([a-zA-Z]+)")
